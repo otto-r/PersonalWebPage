942f1d3f453cccef9b4d7a302111a6cf0f4147fe
Otto <otto.rinaldo@gmail.com>
2018-03-07 16:33:19 +0100

    First commit personal website

---
+++ Otto-js.js
@@ -0,0 +1,40 @@
+function changeBackgroundStripes()
+{   
+    document.getElementById("1").style.backgroundImage = 'url(stripes.png)';
+}
+
+function changeBackgroundTomato()
+{   
+    document.getElementById("1").style.backgroundImage = "";
+    document.getElementById("1").style.backgroundColor = "tomato";
+}
+
+function changeBackgroundBlue()
+{   
+    document.getElementById("1").style.backgroundImage = 'url(none)';
+    document.getElementById("1").style.backgroundColor = "#00bfff";
+}
+
+function changeBackgroundGold()
+{   
+    document.getElementById("1").style.backgroundImage = 'url(none)';
+    document.getElementById("1").style.backgroundColor = "gold";
+}
+
+function changeBackgroundGreen()
+{   
+    document.getElementById("1").style.backgroundImage = 'url(none)';
+    document.getElementById("1").style.backgroundColor = "#90ee90";
+}
+
+function changeBackgroundBlue()
+{   
+    document.getElementById("1").style.backgroundImage = 'url(none)';
+    document.getElementById("1").style.backgroundColor = "#00bfff";
+}
+
+function changeBackgroundColor(string)
+{   
+    document.getElementById("1").style.backgroundImage = 'url(none)';
+    document.getElementById("1").style.backgroundColor = string;
+}
